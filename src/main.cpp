9e1691c61d10d1934b9e4a95c059b8206610d5aa
RINUJAUHAR <rinujauhar@gmail.com>
2025-10-08 07:32:55 +0530

    configuration_updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "🚀 SmartCache Initialized Successfully!" << std::endl;
+    std::cout << " SmartCache Initialized Successfully!" << std::endl;
     return 0;
 }
